ca4b266223173e2b5c8459c19b7bb2dad94a689a
Kevin <kevinaryel.dev@gmail.com>
2023-09-24 15:13:24 -0300

    Configurado o formulario de login com hook-form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,26 +6,46 @@ import {
   Heading,
   Input,
   Stack,
+  Text,
   Box
 } from '@chakra-ui/react'
 import './Login.css'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { loginSchema } from '../utils/schemas'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 const Login = () => {
+
+  const {handleLogin} = useContext(AuthContext)
+
+  const { register, handleSubmit, formState:{errors}} = useForm({resolver:yupResolver(loginSchema)})
+
+
   return (
     <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }} minW={'100vw'}>
       <Flex p={8} flex={1} align={'center'} justify={'center'} id='login-camp'>
-        <Stack spacing={4} w={'full'} maxW={'md'}>
+        <Stack spacing={4} w={'full'} maxW={'md'} as='form' onSubmit={handleSubmit((data)=> handleLogin(data))}>
           <Heading fontSize={'2xl'}>Faça login no nosso sistema!</Heading>
           <FormControl id="email">
             <FormLabel>Usuário</FormLabel>
-            <Input type='text' />
+            <Input type='text' {...register("usuario")} isInvalid={!!errors.usuario} />
+            {!!errors.usuario?
+            <Text color="red" fontSize="sm" margin={'0'}>{errors.usuario.message}</Text>
+            :<></>
+            }
           </FormControl>
           <FormControl id="password">
             <FormLabel>Senha</FormLabel>
-            <Input type="password" />
+            <Input type="password" {...register("senha")} isInvalid={!!errors.senha}/>
+            {!!errors.senha?
+            <Text color="red" fontSize="sm" margin={"0"}>{errors.senha.message}</Text>:
+            <></>
+            }
           </FormControl>
           <Stack spacing={6}>
-            <Button colorScheme={'blue'} variant={'solid'}>
+            <Button colorScheme={'blue'} variant={'solid'} type='submit'>
               Entrar
             </Button>
           </Stack>
