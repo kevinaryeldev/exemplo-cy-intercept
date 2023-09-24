d0b167b38050e81fca9b5ccceea0306120bcdb4d
Kevin <kevinaryel.dev@gmail.com>
2023-09-24 11:48:21 -0300

    Criacao de página de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Box
+} from '@chakra-ui/react'
+import './Login.css'
+
+export default function Home() {
+  return (
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }} minW={'100vw'}>
+      <Flex p={8} flex={1} align={'center'} justify={'center'} id='login-camp'>
+        <Stack spacing={4} w={'full'} maxW={'md'}>
+          <Heading fontSize={'2xl'}>Faça login no nosso sistema!</Heading>
+          <FormControl id="email">
+            <FormLabel>Usuário</FormLabel>
+            <Input type='text' />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Senha</FormLabel>
+            <Input type="password" />
+          </FormControl>
+          <Stack spacing={6}>
+            <Button colorScheme={'blue'} variant={'solid'}>
+              Entrar
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1} maxWidth="full">
+        <Box id="login-image"
+        boxSize="full"
+        width="100%"
+          alt={'Login Image'
+          }
+        />
+      </Flex>
+    </Stack>
+  )
+}
