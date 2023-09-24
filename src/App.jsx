dee6def539a599e03cea87d52b2ddde12c09782e
Kevin <kevinaryel.dev@gmail.com>
2023-09-24 11:48:10 -0300

    Envelopando o contexto de auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { Router, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import PrivateRoutes from './components/PrivateRoutes'
-
+import Login from './pages/Login'
+import { AuthProvider } from './context/AuthContext'
 const App = () => {
 
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<PrivateRoutes />}>
-        </Route>
-      </Routes>
-    </Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route element={<PrivateRoutes />}>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </>
   )
 }
