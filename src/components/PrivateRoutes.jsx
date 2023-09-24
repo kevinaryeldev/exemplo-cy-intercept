b5f5aa613fa2264ec1d20fc62fdd6e4bd2cecc27
Kevin <kevinaryel.dev@gmail.com>
2023-09-24 11:47:20 -0300

    Configuracao da Auth

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,7 +1,25 @@
+import { useContext, useLayoutEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const PrivateRoutes = () => {
-    let auth = localStorage.getItem("auth");
+
+    const {getUserData,token,user} = useContext(AuthContext)
+    let auth = true
+
+    useLayoutEffect(async ()=>{
+        if (user){
+            return
+        } else{
+            if(token){
+                await getUserData();
+                user? auth=true :auth = false
+            }else{
+                auth = false
+            }
+        }
+    });
+
     return( 
         auth ? <Outlet/> : <Navigate to={"/"}/>
     )
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+import api from "../utils/api.js";
+import { useNavigate } from "react-router";
+
+
+export const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+
+    const token = localStorage.getItem("userToken")
+
+    const [user, setUser] = useState();
+    const navigate = useNavigate();
+
+    const handleLogin = async (user) => {
+        try {
+            const { data } = await api.post("/login", user);
+            localStorage.setItem("userToken", JSON.stringify(data));
+            navigate("/home");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    };
+
+    const logout = () => {
+        localStorage.removeItem("userToken");
+        setUser(null);
+        navigate("/");
+    };
+
+    const getUserData = async () => {
+        if (!!token){
+            try {
+                const { data } = await api.get("/user");
+                setUser(data);
+            } catch (error) {
+                toast.error(error.response.data.message);
+                logout();
+            }
+        }
+    };
+
+
+    return (
+        <AuthContext.Provider value={{ user,token, handleLogin, getUserData, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
